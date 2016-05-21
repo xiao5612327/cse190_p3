64c2f2bc7cf0a8cf22a2ae70fb36af4958222092
Eva <e4huynh@ucsd.edu>
2016-05-20 21:59:04 -0700

    kinda finished writing robot.py. Started a*

--- scripts/astar.py
+++ scripts/astar.py
@@ -1,1 +1,45 @@
-# astar implementation needs to go here
+#!/usr/bin/env python
+import rospy
+import random as r
+import math
+import numpy as np
+from read_config import read_config
+from std_msgs.msg import String, Float32, Bool
+import heapq
+#from copy import deepcopy, copy
+
+
+def astar ():
+	self.config = read_config()
+	self.move_list = self.config['move_list']
+	self.start = self.config['start']
+	self.goal = self.config['goal']
+	self.walls = self.config['walls']
+	self.move_list = self.config['pits']
+	self.map_size = self.config['map_size']
+	self.row = self.map_size[0]	
+	self.col = self.map_size[1]	
+
+	self.heuristics_grid = [self.row][self.col]	
+	self.heuristics()
+
+	self.heap = []
+	self.astar_func()
+
+def heuristics ():
+	for i in range (self.row):
+		for j in range (self.col):
+			value1 = math.fabs(i - self.goal[0])
+			value2 = math.fabs(j - self.goal[1])
+			self.heuristics_grid[i][j] = value1 + value2
+
+	
+def astar_func():
+	for i in range (self.row):
+		for j in range (self.col):
+			self.heap.append( (,i,j) )
+
+	
+
+
+
--- scripts/robot.py
+++ scripts/robot.py
@@ -1,3 +1,46 @@
 #!/usr/bin/env python
 
-#robot.py implementation goes here
+import rospy
+import random
+import math
+import numpy as np
+from std_msgs.msg import String, Float32, Bool
+from read_config import read_config
+from astar import *
+from mdp import *
+
+class Robot ():
+	def __init__(self):
+		rospy.init_node ("robot")
+		self.config = read_config()
+		self.astar_pub = rospy.Publisher(
+			"/results/path_list",
+			AStarPath,
+			queue_size = 10
+		)
+
+		self.sim_complete_pub = rospy.Publisher(
+			"/map_node/sim_complete",
+			Bool,
+			queue_size = 10
+		)
+
+		# publish A* path
+		self.publish_astar()
+		mdp()
+
+		rospy.sleep(1)
+		self.sim_complete_pub.publish(True)
+		rospy.sleep(1)
+		rospy.signal_shutdown(Robot)
+
+
+	def publish_astar(self):
+		# astar should return []
+		path_array = astar()
+		for i in range (len(path_array)):
+			self.astar.publish(path_array[i])
+
+
+if __name__ == '__main__':
+   r = Robot()
