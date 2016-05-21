ba137f1451a6e1441dd5fe3b6422c8cac233aa5c
xiaoping weng <xweng@ucsd.edu>
2016-05-21 17:56:03 -0500

    edit astart.py

--- scripts/astar.py
+++ scripts/astar.py
@@ -8,6 +8,14 @@ from std_msgs.msg import String, Float32, Bool
 import heapq
 #from copy import deepcopy, copy
 
+class cell(object):
+	def _ini_(self, x, y, wall, h, uniform_cost):
+		self.x = x
+		self.y = y
+		self.wall = wall
+		self.h = h
+		self.uniform_cost = uniform_cost
+		self.total_cost = h + uniform_cost
 
 def astar ():
 	self.config = read_config()
@@ -17,28 +25,54 @@ def astar ():
 	self.walls = self.config['walls']
 	self.move_list = self.config['pits']
 	self.map_size = self.config['map_size']
-	self.row = self.map_size[0]	
-	self.col = self.map_size[1]	
+	self.row_height = self.map_size[0]	
+	self.col_width = self.map_size[1]	
 
 	self.heuristics_grid = [self.row][self.col]	
 	self.heuristics()
-
+	
+	self.uniform_grid = [self.row][self.col]	
+	self.uniform()
+	
 	self.heap = []
+	self.visited = []
+
 	self.astar_func()
 
-def heuristics ():
+def heuristics (self, cell):
 	for i in range (self.row):
 		for j in range (self.col):
 			value1 = math.fabs(i - self.goal[0])
 			value2 = math.fabs(j - self.goal[1])
 			self.heuristics_grid[i][j] = value1 + value2
 
-	
-def astar_func():
+def uniform(self, cell):
 	for i in range (self.row):
 		for j in range (self.col):
-			self.heap.append( (,i,j) )
+			value1 = math.fabs(i - self.start[0])
+			value2 = math.fabs(j - self.start[1])
+			self.uniform_grid[i][j] = value1 + value2
 
+def get_cell(self, x, y):
+	for i in range (len(self.heap)):
+		if self.heap[i].x == x and self.heap[i].y == y:
+			return self.heap[i]
+
+def astar_func():
+	#inital all celss
+	for i in range (self.row):
+		for j in range (self.col):
+			#check if is wall
+			if (i, j) in walls:
+				wall = True
+			else
+				wall = False
+			#create a temp cell
+			temp_cell = cell(i, j, wall, self.heuristics_graid[i][j], self.uniform_grid[i][j])
+			self.heap.append(temp_cell)
+
+	
+	self.visited.append(self.start)
 	
 
 
